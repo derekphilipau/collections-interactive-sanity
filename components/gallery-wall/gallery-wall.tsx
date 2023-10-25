fc65c417cb8e032fa745fe06e8dd4666046d1873
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-10-25 13:12:50 -0400

    futher simplify, still perf issue

--- components/gallery-wall/gallery-wall.tsx
+++ components/gallery-wall/gallery-wall.tsx
@@ -20,63 +20,64 @@ export default function GalleryWall({ galleryWall }: GalleryWallProps) {
   const [installedArtwork, setInstalledArtwork] =
     useState<InstalledArtwork | null>(null);
 
-  function onImageClick(installedArtwork: InstalledArtwork | null) {
+  function onImageClick(installedArtwork: InstalledArtwork) {
     setInstalledArtwork(installedArtwork);
-    setOpen(true);
+    if (!open) {
+      setOpen(true);
+    }
   }
 
   return (
-    <section className="">
-      <div className="relative h-screen w-screen overflow-x-hidden bg-neutral-800 bg-gradient-to-t from-neutral-900 to-neutral-800">
-        <div
-          className="absolute z-40"
-          style={{
-            bottom: `3%`,
-            left: `2%`,
-          }}
-        >
-          <h1 className="text-white text-xl md:text-3xl lg:text-5xl font-bold tracking-tight leading-tight md:leading-none cursor-pointer uppercase">
-            {getTranslation(galleryWall.title, lang)}
-          </h1>
-        </div>
-        <div
-          className="absolute z-50 text-xl text-muted font-bold tracking-tight leading-tight md:leading-none"
-          style={{
-            bottom: `3%`,
-            right: '3%',
-          }}
-        >
-          {lang !== 'es' && (
-            <Button variant="custom" size="xl" onClick={() => setLang('es')}>
-              Español
-            </Button>
-          )}
-          {lang !== 'en' && (
-            <Button variant="custom" size="xl" onClick={() => setLang('en')}>
-              English
-            </Button>
-          )}
-        </div>
-        {galleryWall.installedArtworks?.length > 0 &&
-          galleryWall.installedArtworks.map(
-            (installedArtwork: InstalledArtwork, i: Key) => (
-              <div
-                className="absolute drop-shadow-lg hover:outline outline-offset-4 hover:outline-1 outline-white cursor-pointer"
-                style={{
-                  width: `${installedArtwork.width}%`,
-                  top: `${installedArtwork.top}%`,
-                  left: `${installedArtwork.left}%`,
-                }}
-                onClick={() => onImageClick(installedArtwork)}
-              >
-                <ThumbnailImage
-                  title={getTranslation(installedArtwork.artwork.title, lang)}
-                  image={installedArtwork.artwork.image}
-                />
-              </div>
-            )
-          )}
+    <div className="relative h-screen w-screen overflow-x-hidden bg-neutral-800 bg-gradient-to-t from-neutral-900 to-neutral-800">
+      <div
+        className="absolute z-40"
+        style={{
+          bottom: `3%`,
+          left: `2%`,
+        }}
+      >
+        <h1 className="text-white text-xl md:text-3xl lg:text-5xl font-bold tracking-tight leading-tight md:leading-none cursor-pointer uppercase">
+          {getTranslation(galleryWall.title, lang)}
+        </h1>
       </div>
+      <div
+        className="absolute z-50 text-xl text-muted font-bold tracking-tight leading-tight md:leading-none"
+        style={{
+          bottom: `3%`,
+          right: '3%',
+        }}
+      >
+        {lang !== 'es' && (
+          <Button variant="custom" size="xl" onClick={() => setLang('es')}>
+            Español
+          </Button>
+        )}
+        {lang !== 'en' && (
+          <Button variant="custom" size="xl" onClick={() => setLang('en')}>
+            English
+          </Button>
+        )}
+      </div>
+      {galleryWall.installedArtworks?.length > 0 &&
+        galleryWall.installedArtworks.map(
+          (installedArtwork: InstalledArtwork, i: Key) => (
+            <div
+              key={i}
+              className="absolute drop-shadow-lg hover:outline outline-offset-4 hover:outline-1 outline-white cursor-pointer"
+              style={{
+                width: `${installedArtwork.width}%`,
+                top: `${installedArtwork.top}%`,
+                left: `${installedArtwork.left}%`,
+              }}
+              onClick={() => onImageClick(installedArtwork)}
+            >
+              <ThumbnailImage
+                title={getTranslation(installedArtwork.artwork.title, lang)}
+                image={installedArtwork.artwork.image}
+              />
+            </div>
+          )
+        )}
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetContent side="left" className="w-1/2 bg-black pt-14">
           <ScrollArea className="h-full overflow-y-auto px-12 pt-2 pb-12 text-white">
@@ -84,6 +85,6 @@ export default function GalleryWall({ galleryWall }: GalleryWallProps) {
           </ScrollArea>
         </SheetContent>
       </Sheet>
-    </section>
+    </div>
   );
 }
